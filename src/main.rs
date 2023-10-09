364544763fa398f8744665f883bed2b8b94d1d1a
Defmc <defmc0101@gmail.com>
2023-10-08 21:45:14 -0400

    fix(qemu): zoom to fit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ fn main() {
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
 
     cmd.arg("-drive")
-        .arg(format!("format=raw,file={bios_path}"));
+        .arg(format!("format=raw,file={bios_path}"))
+        .arg("-display")
+        .arg("gtk,zoom-to-fit=on");
     let mut child = cmd.spawn().unwrap();
     child.wait().unwrap();
 }
