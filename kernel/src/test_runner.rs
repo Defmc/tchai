b5d253fe4074d067f65f9d37375f7495d65ac6c7
Defmc <defmc0101@gmail.com>
2023-10-10 17:45:58 -0400

    test(double_fault): add feature `test_double_fault`

--- kernel/src/test_runner.rs
+++ kernel/src/test_runner.rs
@@ -4,10 +4,10 @@ pub const TESTS: &[(&'static str, fn())] = &[
     ("equality", eq_assertion),
     ("floating point arithmetic", float_arithmetic),
     ("breakpoint exception", breakpoint_exception),
-    // (
-    //     "kernel stackoverflow exception",
-    //     kernel_stackoverflow_exception,
-    // ),
+    (
+        "kernel stackoverflow exception",
+        kernel_stackoverflow_exception,
+    ),
 ];
 
 pub fn run_tests() {
@@ -17,10 +17,9 @@ pub fn run_tests() {
 pub fn executor(tests: &[(&'static str, fn())]) {
     info!("running {} tests", tests.len());
     for (name, test) in tests.iter() {
-        info!("running test '{name}'");
+        info!("\trunning test '{name}'");
         test();
-        okay!("test suceeded");
-        println!("");
+        okay!("\ttest suceeded");
     }
 }
 
@@ -38,13 +37,18 @@ pub fn breakpoint_exception() {
 }
 
 pub fn kernel_stackoverflow_exception() {
-    // #[allow(unconditional_recursion)]
-    // fn overflow() {
-    //     overflow();
-    //     let x = 0;
-    //     unsafe {
-    //         core::ptr::write_volatile(x as *mut u8, 1);
-    //     }
-    // }
-    // overflow()
+    #[cfg(feature = "test_double_fault")]
+    #[allow(unconditional_recursion)]
+    fn overflow() {
+        overflow();
+        let x = 0;
+        unsafe {
+            core::ptr::write_volatile(x as *mut u8, 1);
+        }
+    }
+    #[cfg(not(feature = "test_double_fault"))]
+    fn overflow() {
+        x86_64::instructions::nop()
+    }
+    overflow()
 }
