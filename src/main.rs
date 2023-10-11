5fe355a221d0539206c1e2a749d4ca43274cdf6e
Defmc <defmc0101@gmail.com>
2023-10-10 21:46:46 -0400

    fix(test): add conditional compiling for bootloader linker

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(main_test)]
+
 fn main() {
+    let bios_path = env!("BIOS_PATH");
+
+    let mut cmd = std::process::Command::new("qemu-system-x86_64");
+
+    cmd.arg("-drive")
+        .arg(format!("format=raw,file={bios_path}"))
+        .arg("-display")
+        .arg("gtk,zoom-to-fit=on");
+    let mut child = cmd.spawn().unwrap();
+    child.wait().unwrap();
+}
+
+#[cfg(test)]
+fn main_test(_: &[&()]) {
     let bios_path = env!("BIOS_PATH");
 
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
